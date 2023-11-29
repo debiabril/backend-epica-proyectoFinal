3fc00dba7f1bf936255336df7b468aa1a58cfd9f
debiabril <debora.badosa@gmail.com>
2023-11-29 12:37:42 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { app } from "./app.js";
+import { settingDotEnvPort } from "./config/dotenv.js";
+
+const { port } = settingDotEnvPort()
+
+app.listen(port, console.log(`Servidor en puerto ${port}`)); 
